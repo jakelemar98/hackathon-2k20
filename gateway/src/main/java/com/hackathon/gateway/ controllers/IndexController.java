ac25b9e47caf085723f082b7557e4d8106f9dfbd
Jake Lemar <jalema01@wsc.edu>
2020-07-09 08:16:58 -0500

    startin

---
+++ gateway/src/main/java/com/hackathon/gateway/ controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.hackathon.gateway.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Spring Boot is running!";
+	}
+
+}
