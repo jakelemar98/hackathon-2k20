b763f69a240264aca884c97ed747ed83bf87d96d
Jake Lemar <jalema01@wsc.edu>
2020-07-09 13:50:48 -0500

    more commits then mark

---
+++ gateway/src/main/java/com/hackathon/gateway/ controllers/UsersController.java
@@ -0,0 +1,17 @@
+package com.hackathon.gateway.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class UsersController {
+
+	@GetMapping("/users/statistics")
+	public ResponseEntity<String> getStatistics() {
+        return new ResponseEntity<>("Yo mark", HttpStatus.OK);
+	}
+
+}
