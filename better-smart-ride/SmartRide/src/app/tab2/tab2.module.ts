ecf6fd1823aeaaf4cd8887c61c5a627604ac76db
Spencer Parry <sparry997@gmail.com>
2020-07-09 22:34:17 -0500

    Tabs 2 & 3

--- better-smart-ride/SmartRide/src/app/tab2/tab2.module.ts
+++ better-smart-ride/SmartRide/src/app/tab2/tab2.module.ts
@@ -1,6 +1,6 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
@@ -16,9 +16,11 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule,
+    Tab2PageRoutingModule
     
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule {
+ 
+}
